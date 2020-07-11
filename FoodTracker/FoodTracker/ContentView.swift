8ed2c92319a32dc67c224afe3ed0a7901d5219a9
mthota23 <66144885+mthota23@users.noreply.github.com>
2020-07-11 12:15:04 -0500

    Add files via upload

---
+++ FoodTracker/FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Meghana Thota on 7/11/20.
+//  Copyright © 2020 Meghana Thota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
